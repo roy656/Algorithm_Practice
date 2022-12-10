a2cd8b6d0721ae87f10c475c4326eb83d97073c6
Roy C <v0o0605@gmail.com>
2022-12-11 00:01:38 +0900

    Fast Campus - 정렬알고리즘_힙정렬 (HeapSort)

--- src/algorithm/facam_algorithm/sorting_algorithm/heap_sort/HeapSort.java
+++ src/algorithm/facam_algorithm/sorting_algorithm/heap_sort/HeapSort.java
@@ -36,7 +36,7 @@ public class HeapSort {
         temp = heapArr[SIZE];
         SIZE -= 1;
         parent =1; child = 2;
-//11
+
         while(child <= SIZE){
             //if((child < HEAP_SIZE) && (heapArr[child] < heapArr[child+1])){ //max heap
             if((child < SIZE) && (heapArr[child] > heapArr[child+1])){ //min heap
